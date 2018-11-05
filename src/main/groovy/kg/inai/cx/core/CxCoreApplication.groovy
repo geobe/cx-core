c7db6ee17468b67dd09781d4e3e2d8d1898266d9
geobe <code@georg.beier.de>
2018-11-05 18:57:37 +0100

    initial commit

---
+++ src/main/groovy/kg/inai/cx/core/CxCoreApplication.groovy
@@ -0,0 +1,12 @@
+package kg.inai.cx.core
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CxCoreApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run CxCoreApplication, args
+    }
+}
---
+++ src/test/groovy/kg/inai/cx/core/CxCoreApplicationTests.groovy
@@ -0,0 +1,16 @@
+package kg.inai.cx.core
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class CxCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
