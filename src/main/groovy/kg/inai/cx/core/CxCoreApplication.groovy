3152711a9835efbc8a438e498c4bded8d47f0698
geobe <code@georg.beier.de>
2018-11-06 13:07:49 +0100

    updated cx-core module

--- src/main/groovy/kg/inai/cx/core/CxCoreApplication.groovy
+++ src/main/groovy/kg/inai/cx/core/CxCoreApplication.groovy
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+/**
+ * This is the main class
+ */
 class CxCoreApplication {
 
     static void main(String[] args) {
